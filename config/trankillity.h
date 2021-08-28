538574261df66199a121366f47ffd41e7d2e3e04
trankillity <trankillity@gmail.com>
2021-08-29 08:17:29 +1000

    Tweak timings/combos
    Added per combo timeout, tweaked hold-tap timing.

--- config/trankillity.h
+++ config/trankillity.h
@@ -7,18 +7,18 @@
 #define NAV_ENT _NAVI ENTER
 #define NUM_BCK _NUMB BACKSPACE
 
-#define X_SELA LC(A)
-#define X_UNDO LC(Z)
-#define X_CUT LC(X)
-#define X_COPY LC(C)
-#define X_PAST LC(V)
-#define X_REDO LC(Y)
+#define X_SELA      LC(A)
+#define X_UNDO      LC(Z)
+#define X_CUT       LC(X)
+#define X_COPY      LC(C)
+#define X_PAST      LC(V)
+#define X_REDO      LC(Y)
 
-#define X_A LGUI A
-#define X_R LALT R
-#define X_S LSHFT S
-#define X_T LCTRL T
-#define X_N RCTRL N
-#define X_E RSHFT E
-#define X_I LALT I
-#define X_O RGUI O
+#define X_A LGUI    A
+#define X_R LALT    R
+#define X_S LCTRL   S
+#define X_T LSHFT   T
+#define X_N RSHFT   N
+#define X_E RCTRL   E
+#define X_I LALT    I
+#define X_O RGUI    O
