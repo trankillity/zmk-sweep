e42aee716a12b2612d506f7dc61f123dff20ac9b
trankillity <trankillity@gmail.com>
2021-08-28 21:38:12 +1000

    Initial Keymap
    Basic keymap setup to test actions.

---
+++ config/trankillity.h
@@ -0,0 +1,24 @@
+#define _BASE   0
+#define _NUMB   1
+#define _FUNC   2
+#define _NAVI   3
+#define _MOUS   4
+#define _SYST   5
+
+#define NAV_ENT _NAVI ENTER
+#define NUM_BCK _NUMB BACKSPACE
+
+#define X_UNDO  C(Z)
+#define X_CUT   C(X)
+#define X_COPY  C(C)
+#define X_PAST  C(V)
+#define X_REDO  C(Y)
+
+#define X_A     LGUI A
+#define X_R     LALT R
+#define X_S     LSHFT S
+#define X_T     LCTRL T
+#define X_N     RCTRL N
+#define X_E     RSHFT E
+#define X_I     LALT I
+#define X_O     RGUI O
